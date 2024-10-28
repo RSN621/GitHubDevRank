59028124a9853756c68a06e15fe6783b8bc4576d
zhaoge <3400223562@qq.com>
2024-10-28 20:54:46 +0800

    查询用户数据 查询仓库数据 查询用户仓库数据

--- src/main/java/org/zhr/githubdevrank/githubapi/githubApi.java
+++ src/main/java/org/zhr/githubdevrank/githubapi/githubApi.java
@@ -1,17 +1,11 @@
 package org.zhr.githubdevrank.githubapi;
 
-import com.jcabi.github.Coordinates;
-import com.jcabi.github.Github;
-import com.jcabi.github.Repo;
-import com.jcabi.github.RtGithub;
-
+import com.jcabi.github.*;
 import java.io.IOException;
 
 public class githubApi {
     public static void main(String[] args) throws IOException {
-        Github github=new RtGithub();
-        Repo repo = github.repos().get(new Coordinates.Simple("rsn621", "carefree"));
-        System.out.println(repo.stars());
-        System.out.println(repo);
+        Github github=new RtGithub("");
+
     }
 }
